a2f7d1dc7ab4542e786abee7d71c4c555a897bff
Wei Jie Phua <weijie_luvbb@hotmail.com>
2023-10-08 21:45:28 +0800

    customize authenticator ui

--- src/App.js
+++ src/App.js
@@ -4,31 +4,54 @@ import SiteNav from './components/Common/SiteNav';
 import SiteFooter from './components/Common/SiteFooter';
 import HomePage from './components/home/HomePage';
 import { Route, Routes } from 'react-router-dom';
-import LoginPage from './components/auth/LoginPage';
-import RegisterPage from './components/auth/RegisterPage';
 import Contacts from './components/contacts/Contacts';
 
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, useTheme, View, Image,Text } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import awsExports from './aws-exports'
 
 Amplify.configure(awsExports)
 
 function App () {
+  const components = {
+    Header () {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Amplify logo"
+            src="https://docs.amplify.aws/assets/logo-dark.svg"
+          />
+        </View>
+      );
+    },
+    Footer () {
+      const { tokens } = useTheme();
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; All Rights Reserved
+          </Text>
+        </View>
+      );
+    },
+  }
+
   return (
-    <Authenticator loginMechanisms={['email']}>
-    <div>
-      <SiteNav />
-      <Routes>
-        <Route path='*' element={<HomePage />} />
-        <Route path='/' exact={true} element={<HomePage />} />
-        <Route path='/login' exact={true} element={<LoginPage />} />
-        <Route path='/register' exact={true} element={<RegisterPage />} />
-        <Route path='/contact' exact={true} element={<Contacts />} />
-      </Routes>
-      <SiteFooter />
-      </div>
+    <Authenticator loginMechanisms={['email']} components={components}>
+      {({ signOut, user }) => (
+        <div>
+          <SiteNav logout={ signOut} />
+          <Routes>
+            <Route path='*' element={<HomePage />} />
+            <Route path='/' exact={true} element={<HomePage />} />
+            <Route path='/contacts' exact={true} element={<Contacts />} />
+          </Routes>
+          <SiteFooter />
+        </div>
+      )}
     </Authenticator>
   );
 }
