a0c44dafce8f69e00ec55c97815986fe19c933b7
Wei Jie Phua <weijie_luvbb@hotmail.com>
2023-10-08 20:59:40 +0800

    added contacts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './components/home/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './components/auth/LoginPage';
 import RegisterPage from './components/auth/RegisterPage';
+import Contacts from './components/contacts/Contacts';
 
 function App () {
   return (
@@ -16,6 +17,7 @@ function App () {
         <Route path='/' exact={true} element={<HomePage />} />
         <Route path='/login' exact={true} element={<LoginPage />} />
         <Route path='/register' exact={true} element={<RegisterPage />} />
+        <Route path='/contact' exact={true} element={<Contacts />} />
       </Routes>
       <SiteFooter />
     </div>
